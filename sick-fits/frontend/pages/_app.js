fc14bdd52cdf8808c674afd761d17a5abd778717
Vee Uye <vee.uye@gmail.com>
2022-10-26 15:04:30 +0100

    feat: add visual progress bar on route change

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,5 +1,14 @@
-/* eslint-disable react/jsx-props-no-spreading */
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+// TODO Swap with our own
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
     return (
--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,14 +1,15 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
