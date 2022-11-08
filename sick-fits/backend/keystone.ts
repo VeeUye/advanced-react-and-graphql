1398845ee7e2b6d13565a6ecfdd36f82bdd492fb
Vee Uye <vee.uye@gmail.com>
2022-11-08 17:00:07 +0000

    feat: hook up seed data to build

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -4,6 +4,7 @@ import { withItemData, statelessSessions } from '@keystone-next/keystone/session
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 import 'dotenv/config';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -34,7 +35,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO add data seeding here
+      async onConnect(keystone) {
+        console.log('connected to the database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here
