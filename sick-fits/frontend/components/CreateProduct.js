ce51ac41e15509807c8f5c5946d3908d1cef74fe
Vee Uye <vee.uye@gmail.com>
2022-11-09 16:17:24 +0000

    feat: programmatically change to product's page after product creation

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,6 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import { ALL_PRODUCTS_QUERY } from './Products';
 import useForm from '../lib/useForm';
 import DisplayError from './ErrorMessage';
@@ -41,11 +42,13 @@ export default function CreateProduct() {
         <Form
             onSubmit={async (e) => {
                 e.preventDefault();
-                console.log(inputs);
-
                 // Submit the input fields to the backend
-                await createProduct();
+                const res = await createProduct();
                 clearForm();
+                // Go to that product's page
+                Router.push({
+                    pathname: `/product/${res.data.createProduct.id}`,
+                });
             }}
         >
             <DisplayError error={error} />
