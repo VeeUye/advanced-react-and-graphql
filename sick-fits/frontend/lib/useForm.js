d40cc9f897672e70395c717c8a5d9020e66f23ba
Vee Uye <vee.uye@gmail.com>
2022-11-09 14:40:54 +0000

    feat: add custom hook for form inputs

---
+++ sick-fits/frontend/lib/useForm.js
@@ -0,0 +1,37 @@
+import { func } from 'prop-types';
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+    const [inputs, setInputs] = useState(initial);
+
+    function handleChange(e) {
+        let { value, name, type } = e.target;
+
+        if (type === 'number') {
+            value = parseInt(value);
+        }
+
+        if (type === 'file') {
+            value[0] = e.target.files;
+        }
+
+        setInputs({ ...inputs, [name]: value });
+    }
+
+    function resetForm() {
+        setInputs(initial);
+    }
+
+    function clearForm() {
+        const blankState = Object.fromEntries(Object.entries(inputs).map(([key, value]) => [key, '']));
+
+        setInputs(blankState);
+    }
+
+    return {
+        inputs,
+        handleChange,
+        resetForm,
+        clearForm,
+    };
+}
