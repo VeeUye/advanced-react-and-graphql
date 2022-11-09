867eb3d6944e7e70c8fb39fdb538389f1ee88861
Vee Uye <vee.uye@gmail.com>
2022-11-09 16:01:34 +0000

    feat: add functionality to create Products using mutation

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,25 +1,67 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
 
+const CREATE_PRODUCT_MUTATION = gql`
+    mutation CREATE_PRODUCT_MUTATION($name: String!, $description: String!, $price: Int!, $image: Upload) {
+        createProduct(
+            data: {
+                name: $name
+                description: $description
+                price: $price
+                status: "AVAILABLE"
+                photo: { create: { image: $image, altText: $name } }
+            }
+        ) {
+            id
+            price
+            description
+            name
+        }
+    }
+`;
+
 export default function CreateProduct() {
-    const { inputs, handleChange, clearForm, resetForm } = useForm({
+    const { inputs, handleChange, clearForm } = useForm({
         image: '',
         name: 'Nice Shoes',
         price: 3456,
         description: 'best shoes ever',
     });
 
+    const [createProduct, { loading, error, data }] = useMutation(CREATE_PRODUCT_MUTATION, {
+        variables: inputs,
+    });
+
     return (
         <Form
-            onSubmit={(e) => {
+            onSubmit={async (e) => {
                 e.preventDefault();
                 console.log(inputs);
+
+                // Submit the input fields to the backend
+                await createProduct();
+                clearForm();
             }}
         >
-            <fieldset>
+            <DisplayError error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
                 <label htmlFor="image">
                     Image
                     <input required type="file" id="image" name="image" onChange={handleChange} />
+                </label>
+                <label htmlFor="name">
+                    Name
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                        value={inputs.name}
+                        onChange={handleChange}
+                    />
                 </label>
                 <label htmlFor="price">
                     Price
