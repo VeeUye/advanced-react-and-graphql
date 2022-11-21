e8848cd252b4f5e2aa85854ff6d86d79c9d9b5f7
Vee Uye <vee.uye@gmail.com>
2022-11-21 16:40:54 +0000

    feat: update pagination query with correct values

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,43 +1,49 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
-import Product from './Product';
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Product from "./Product";
+import { perPage } from "../config";
 
 export const ALL_PRODUCTS_QUERY = gql`
-    query ALL_PRODUCTS_QUERY {
-        allProducts {
-            id
-            name
-            price
-            description
-            photo {
-                id
-                image {
-                    publicUrlTransformed
-                }
-            }
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(skip: $skip, first: $first) {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
         }
+      }
     }
+  }
 `;
 
 const ProductsListStyles = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 60px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
 `;
 
-export default function Products() {
-    const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
-    console.log(data, error, loading);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>{error.message}</p>;
-    return (
-        <div>
-            <ProductsListStyles>
-                {data.allProducts.map((product) => (
-                    <Product key={product.id} product={product} />
-                ))}
-            </ProductsListStyles>
-        </div>
-    );
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
+  console.log(data, error, loading);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+  return (
+    <div>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsListStyles>
+    </div>
+  );
 }
