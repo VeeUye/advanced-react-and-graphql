de000ca1e26bb64ccf8d4951915cd17d3a9e8fdc
Vee Uye <vee.uye@gmail.com>
2022-11-21 16:25:09 +0000

    feat: enable dynamic pagination

---
+++ sick-fits/frontend/pages/products/index.js
@@ -0,0 +1,18 @@
+import Products from "../../components/Products";
+import Pagination from "../../components/Pagination";
+import { useRouter } from "next/router";
+
+function OrderPage() {
+  const { query } = useRouter();
+  const page = parseInt(query.page);
+
+  return (
+    <div>
+      <Pagination page={page || 1} />
+      <Products />
+      <Pagination page={page || 1} />
+    </div>
+  );
+}
+
+export default OrderPage;
