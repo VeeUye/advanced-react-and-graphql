5c3c538586e4e2034033c2f412d74715080a4b13
Vee Uye <vee.uye@gmail.com>
2022-11-22 13:39:24 +0000

    feat: create sign out component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import { useUser } from "./User";
+import SignOut from "./SignOut";
 
 export default function Nav() {
   const user = useUser();
@@ -13,6 +14,7 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       )}
       {!user && (
