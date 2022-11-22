2e3d0858d6cc9f9143664b21037eb45d0eef6320
Vee Uye <vee.uye@gmail.com>
2022-11-22 16:10:59 +0000

    feat: create reset page and component

---
+++ sick-fits/frontend/components/Reset.js
@@ -0,0 +1,89 @@
+import Form from "./styles/Form";
+import Error from "./ErrorMessage";
+import useForm from "../lib/useForm";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import { CURRENT_USER_QUERY } from "./User";
+
+const RESET_MUTATION = gql`
+  mutation RESET_MUTATION(
+    $email: String!
+    $token: String!
+    $password: String!
+  ) {
+    redeemUserPasswordResetToken(
+      email: $email
+      token: $token
+      password: $password
+    ) {
+      code
+      message
+    }
+  }
+`;
+
+export default function Reset() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: "",
+    password: "",
+    token: "",
+  });
+
+  const [reset, { data, loading }] = useMutation(RESET_MUTATION, {
+    variables: inputs,
+  });
+
+  const error = data?.redeemUserPasswordResetToken.code
+    ? data?.redeemUserPasswordResetToken
+    : undefined;
+
+  console.log(error);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(inputs);
+    const res = await reset().catch(console.error);
+    console.log(res);
+    console.log({ data, loading, error });
+    resetForm();
+    // send the email and password to the graphql api
+  }
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Reset Your Password</h2>
+      <Error error={error} />
+      <fieldset>
+        {data?.redeemUserPasswordResetToken === null && (
+          <p>Success! You can now sign in</p>
+        )}
+
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Your Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Request Reset</button>
+      </fieldset>
+    </Form>
+  );
+}
---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,20 @@
+import RequestReset from "../components/RequestReset";
+import Reset from "../components/Reset";
+
+export default function ResetPage({ query }) {
+  if (!query?.token) {
+    return (
+      <div>
+        <p>Sorry, you must supply a token</p>
+        <RequestReset />
+      </div>
+    );
+  }
+  console.log(query.token);
+  return (
+    <div>
+      <p>Reset Your Password {query.token}</p>
+      <Reset />
+    </div>
+  );
+}
