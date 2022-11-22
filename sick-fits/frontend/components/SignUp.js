e985977cfc540d63a7bdeb2ef6141eb5f163a5f0
Vee Uye <vee.uye@gmail.com>
2022-11-22 14:34:30 +0000

    feat: create sign up component

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -5,57 +5,73 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 import { CURRENT_USER_QUERY } from "./User";
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGN_IN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGN_UP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { name: $name, email: $email, password: $password }) {
+      id
+      email
+      name
     }
   }
 `;
 
-export default function SignIn() {
+export default function SignUp() {
   const { inputs, handleChange, resetForm } = useForm({
     email: "",
+    name: "",
     password: "",
   });
 
-  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
     variables: inputs,
     // refetch the currently logged in user
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
 
   async function handleSubmit(e) {
     e.preventDefault();
     console.log(inputs);
-    const res = await signin();
+    const res = await signup().catch(console.error);
     console.log(res);
+    console.log({ data, loading, error });
     resetForm();
     // send the email and password to the graphql api
   }
-  const error =
-    data?.authenticateUserWithPassword.__typename ===
-    "UserAuthenticationWithPasswordFailure"
-      ? data?.authenticateUserWithPassword
-      : undefined;
-
-  console.log(error);
+  // const error =
+  //   data?.authenticateUserWithPassword.__typename ===
+  //   "UserAuthenticationWithPasswordFailure"
+  //     ? data?.authenticateUserWithPassword
+  //     : undefined;
+  //
+  // console.log(error);
 
   return (
     <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign Into Your Account</h2>
+      <h2>Sign Up For An Account</h2>
       <Error error={error} />
       <fieldset>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please go ahead and sign
+            in!
+          </p>
+        )}
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+
         <label htmlFor="email">
           Email
           <input
--- sick-fits/frontend/pages/signin.js
+++ sick-fits/frontend/pages/signin.js
@@ -1,8 +1,18 @@
 import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+import styled from "styled-components";
+
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
+
 export default function SignInPage() {
   return (
-    <div>
+    <GridStyles>
       <SignIn />
-    </div>
+      <SignUp />
+    </GridStyles>
   );
 }
