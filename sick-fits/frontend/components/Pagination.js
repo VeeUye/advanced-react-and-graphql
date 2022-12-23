a8c4df32352a6cda87e03ef8814b3509c6883690
Vee Uye <vee.uye@gmail.com>
2022-12-23 13:52:08 +0000

    fix: correct pagination display in browser tab

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -26,7 +26,9 @@ export default function Pagination({ page }) {
   return (
     <PaginationStyles>
       <Head>
-        <title>Sick Fits - Page {page} of ___</title>
+        <title>
+          Sick Fits - Page {page} of {pageCount}
+        </title>
       </Head>
       <Link href={`/products/${page - 1}`}>
         <a aria-disabled={page <= 1}>{"\u2190 Prev"}</a>
